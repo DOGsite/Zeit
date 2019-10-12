dd00d533bd7dccee8bd9ef3ca770a6f88ac5f3a9
DOGsite <45077261+DOGsite@users.noreply.github.com>
2019-10-12 23:34:19 +0300

    time2.0

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ setInterval(function(){
 	let hours = 23 - now.getHours();
 	let minutes = 59 - now.getMinutes();
 	let seconds = 60 - now.getSeconds();
+	if(hours<10){
+   hours = "0" + hours;
+	}
+	if(minutes<10){
+		minutes = "0" + minutes;
+		 }
+		 if(seconds<10){
+			seconds = "0" + seconds;
+			 }
 	block.innerHTML =   hours + ":" + minutes + ":" + seconds;
+	
 },1000)
 
 let p = document.querySelector('.timenow');
 setInterval(function(){
 	let now = new Date();
-	p.innerHTML =   now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+	if(hours<10){
+		hours = "0" + hours;
+		 }
+		 if(minutes<10){
+			 minutes = "0" + minutes;
+			  }
+			  if(seconds<10){
+				 seconds = "0" + seconds;
+				  }
+		p.innerHTML =   hours + ":" + minutes + ":" + seconds;
+	
 },1000)
+
 
 // if(seconds > 0){
 //     seconds--;
